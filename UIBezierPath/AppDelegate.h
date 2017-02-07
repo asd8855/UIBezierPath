79b806647fa8f3ff7e24d7d3806b03244102f8ac
asd <asd13148866@sina.com>
2017-02-07 09:51:02 +0800

    Initial Commit

---
+++ UIBezierPath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIBezierPath
+//
+//  Created by libo on 2017/2/7.
+//  Copyright © 2017年 蝉鸣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
